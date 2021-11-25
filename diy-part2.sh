233712931acf1ca728269b9eae64bd750ef4b480
arbalest <57560360+arbaleast@users.noreply.github.com>
2021-11-25 20:25:03 +0800

    Update diy-part2.sh
    uncommnet admin address change

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.98.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.98.1/g' package/base-files/files/bin/config_generate
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git   package/luci-theme-argon
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome 
